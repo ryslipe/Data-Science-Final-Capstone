2535eb7467d79a597d04eaabca09ccc66d4f0434
ryslipe <125102205+ryslipe@users.noreply.github.com>
2024-07-04 13:53:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -315,7 +315,7 @@ if selected == 'Quarterbacks':
 
     
     if choice:
-        fig3 = st.pyplot_chart(full_graph(choice, master_set))
+        fig3 = st.plotly_chart(full_graph(choice, master_set))
         
         
     
