93a2b33c8b270ae70e195f69f13d92d79babe0d3
ryslipe <125102205+ryslipe@users.noreply.github.com>
2024-07-04 12:17:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -283,33 +283,33 @@ if selected == 'Quarterbacks':
     df_final = df.copy()
     import plotly.graph_objects as go
     def full_graph(player, master_set):
-    '''Function to graph a player's actual from training and projected from testing.'''
-    # Filter data for the specified player
-    actual = master_set.loc[master_set['player_display_name'] == player]
-    actual.reset_index(inplace=True)
-    actual['index'] = actual.index
-
-    # Extract actual and projected values
-    y_vals = actual['fantasy_points_ppr']
-    test_projections = actual['predicted']
-
-    # Create a Plotly figure
-    fig = go.Figure()
-
-    # Add traces for actual and projected points
-    fig.add_trace(go.Scatter(x=actual['period'], y=y_vals,
-                             mode='lines+markers',
-                             name='Actual'))
-    fig.add_trace(go.Scatter(x=actual['period'], y=test_projections,
-                             mode='lines+markers',
-                             name='Projected'))
-
-    # Customize the figure
-    fig.update_layout(title=f"{player}'s Fantasy Points",
-                      xaxis_title='Period',
-                      yaxis_title='Fantasy Points')
-
-    return fig
+        '''Function to graph a player's actual from training and projected from testing.'''
+        # Filter data for the specified player
+        actual = master_set.loc[master_set['player_display_name'] == player]
+        actual.reset_index(inplace=True)
+        actual['index'] = actual.index
+    
+        # Extract actual and projected values
+        y_vals = actual['fantasy_points_ppr']
+        test_projections = actual['predicted']
+    
+        # Create a Plotly figure
+        fig = go.Figure()
+    
+        # Add traces for actual and projected points
+        fig.add_trace(go.Scatter(x=actual['period'], y=y_vals,
+                                 mode='lines+markers',
+                                 name='Actual'))
+        fig.add_trace(go.Scatter(x=actual['period'], y=test_projections,
+                                 mode='lines+markers',
+                                 name='Projected'))
+    
+        # Customize the figure
+        fig.update_layout(title=f"{player}'s Fantasy Points",
+                          xaxis_title='Period',
+                          yaxis_title='Fantasy Points')
+    
+        return fig
 
     
     if choice:
